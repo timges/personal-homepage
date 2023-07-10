3d52327472e5b949d4a43863085a5104dbe8cc8c
Tim Gesemann <tim@gesemann.dev>
2023-07-10 13:57:50 +0200

    :arrow_up: migrate to svelte v4

--- src/lib/components/socials.svelte
+++ src/lib/components/socials.svelte
@@ -5,8 +5,8 @@
 
 <aside>
 	{#if showBackToTop}
-		<div class="back-to-top" in:slide out:slide>
-			<button on:click={() => window.scrollTo({top})}>
+		<div class="back-to-top" in:slide|global out:slide|global>
+			<button on:click={() => window.scrollTo({top: 0})}>
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
 					><path d="M13,20H11V8L5.5,13.5L4.08,12.08L12,4.16L19.92,12.08L18.5,13.5L13,8V20Z" /></svg
 				>
