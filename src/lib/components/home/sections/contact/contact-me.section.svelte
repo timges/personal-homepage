4944cf051ef9b995e27bc67bbe5fe08147b56242
Tim Gesemann <tim.gesemann@volkswagen.de>
2024-03-01 12:29:00 +0100

    Add loading indicator and server error toast to contact form

--- src/lib/components/home/sections/contact/contact-me.section.svelte
+++ src/lib/components/home/sections/contact/contact-me.section.svelte
@@ -7,20 +7,27 @@
 	import { tick } from 'svelte';
 	import { PUBLIC_RECAPTCHA_SITE_KEY } from '$env/static/public';
 	import { cookieStatus } from '$lib/store/cookie-store';
+	import LoadingIndicator from '$lib/components/loading-indicator.svelte';
+	import Toast from '$lib/components/toast.svelte';
 
 	export let data: { form: SuperValidated<any> };
 
 	let formRef: HTMLFormElement;
+	let isSubmitting = false;
+	let submissionError = false;
 
+	$: submissionError && setTimeout(() => (submissionError = false), 2000);
 	$: $form.consent ? ($cookieStatus = 'accepted') : null;
 	const { form, enhance, errors } = superForm(data.form, {
 		validationMethod: 'auto',
 		validators: contactFormSchema,
 		onResult: ({ result }) => {
-			console.log(result.status);
+			isSubmitting = false;
 			if (result.status === 200) {
 				submissionSuccessful = true;
+				return;
 			}
+			submissionError = true;
 			if (result.status === 401) {
 				console.log('invalid captcha');
 			}
@@ -40,6 +47,7 @@
 			grecaptcha.execute(PUBLIC_RECAPTCHA_SITE_KEY, { action: 'submit' }).then(async function (
 				token: string
 			) {
+				isSubmitting = true;
 				$form.token = token;
 				await tick();
 				formRef.requestSubmit();
@@ -87,39 +95,44 @@
 				bind:checked={$form.consent}
 			/>
 		</div>
-		<button
-			type="submit"
-			style:background-color={submissionSuccessful ? 'green' : undefined}
-			on:click|preventDefault={onSubmitButtonClick}
-			disabled={!$form.consent}
-		>
-			<div class="button-content">
-				{#if submissionSuccessful}
-					<svg
-						in:fly
-						xmlns="http://www.w3.org/2000/svg"
-						xml:space="preserve"
-						viewBox="0 0 469.184 469.185"
-					>
-						<path
-							d="m462.5 96.193-21.726-21.726c-8.951-8.95-23.562-8.95-32.59 0L180.368 302.361l-119.34-119.34c-8.95-8.951-23.562-8.951-32.589 0L6.712 204.747c-8.95 8.951-8.95 23.562 0 32.589L163.997 394.62c4.514 4.514 10.327 6.809 16.218 6.809s11.781-2.295 16.219-6.809L462.27 128.783c9.18-9.103 9.18-23.638.23-32.59z"
-						/>
-					</svg>
-				{:else}
-					<div in:fly>
-						Send <svg
+		<div class="submit-contaienr">
+			{#if isSubmitting}
+				<LoadingIndicator />
+			{/if}
+			<button
+				type="submit"
+				style:background-color={submissionSuccessful ? 'green' : undefined}
+				on:click|preventDefault={onSubmitButtonClick}
+				disabled={!$form.consent}
+			>
+				<div class="button-content">
+					{#if submissionSuccessful}
+						<svg
+							in:fly
 							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 495.003 495.003"
 							xml:space="preserve"
+							viewBox="0 0 469.184 469.185"
 						>
 							<path
-								d="M164.711 456.687a8.007 8.007 0 0 0 4.266 7.072 7.992 7.992 0 0 0 8.245-.468l55.09-37.616-67.6-32.22v63.232zM492.431 32.443a8.024 8.024 0 0 0-5.44-2.125 7.89 7.89 0 0 0-3.5.816L7.905 264.422a14.154 14.154 0 0 0 .153 25.472L133.4 349.618l250.62-205.99-219.565 220.786 156.145 74.4a14.115 14.115 0 0 0 6.084 1.376c1.768 0 3.519-.322 5.186-.977a14.188 14.188 0 0 0 7.97-7.956l154.596-390a7.968 7.968 0 0 0-2.005-8.814z"
+								d="m462.5 96.193-21.726-21.726c-8.951-8.95-23.562-8.95-32.59 0L180.368 302.361l-119.34-119.34c-8.95-8.951-23.562-8.951-32.589 0L6.712 204.747c-8.95 8.951-8.95 23.562 0 32.589L163.997 394.62c4.514 4.514 10.327 6.809 16.218 6.809s11.781-2.295 16.219-6.809L462.27 128.783c9.18-9.103 9.18-23.638.23-32.59z"
 							/>
 						</svg>
-					</div>
-				{/if}
-			</div>
-		</button>
+					{:else}
+						<div in:fly>
+							Send <svg
+								xmlns="http://www.w3.org/2000/svg"
+								viewBox="0 0 495.003 495.003"
+								xml:space="preserve"
+							>
+								<path
+									d="M164.711 456.687a8.007 8.007 0 0 0 4.266 7.072 7.992 7.992 0 0 0 8.245-.468l55.09-37.616-67.6-32.22v63.232zM492.431 32.443a8.024 8.024 0 0 0-5.44-2.125 7.89 7.89 0 0 0-3.5.816L7.905 264.422a14.154 14.154 0 0 0 .153 25.472L133.4 349.618l250.62-205.99-219.565 220.786 156.145 74.4a14.115 14.115 0 0 0 6.084 1.376c1.768 0 3.519-.322 5.186-.977a14.188 14.188 0 0 0 7.97-7.956l154.596-390a7.968 7.968 0 0 0-2.005-8.814z"
+								/>
+							</svg>
+						</div>
+					{/if}
+				</div>
+			</button>
+		</div>
 		<div class="re-captcha-disclaimer">
 			This site is protected by reCAPTCHA and the Google
 			<a target="_blank" href="https://policies.google.com/privacy">Privacy Policy</a> and
@@ -131,6 +144,7 @@
 		have enriching discussions. Your message could spark something wonderful—let's start a dialogue!
 	</p>
 </section>
+<Toast message="Oops.. Something went wrong. Please try again later" display={submissionError} />
 
 <style lang="scss">
 	@import '../../../../../sass/main';
@@ -252,6 +266,13 @@
 				background-color: var(--color-primary);
 			}
 		}
+	}
+
+	.submit-contaienr {
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
+		gap: var(--spacing-m);
 	}
 
 	@media screen and (max-width: 768px) {
---
+++ src/lib/components/toast.svelte
@@ -0,0 +1,52 @@
+<script>
+	import { fly } from 'svelte/transition';
+
+	export let severity = 'error';
+	export let message = 'This is a toast message';
+	export let display = false;
+
+	function hideToast() {
+		display = false;
+	}
+</script>
+
+{#if display}
+	<aside class={severity} transition:fly>
+		<p>{message}</p>
+		<button on:click={hideToast}>X</button>
+	</aside>
+{/if}
+
+<style lang="scss">
+	@import '../../sass/main';
+
+	aside {
+		position: fixed;
+		bottom: var(--spacing-l);
+		padding: var(--spacing-m);
+		left: 50%;
+		transform: translateX(-50%);
+		border-radius: var(--border-radius-s);
+		background-color: var(--color-error);
+		box-shadow: var(--shadow);
+		z-index: 100;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		gap: var(--spacing-xl);
+		color: var(--color-text-on-primary);
+		font-size: var(--font-size-b1);
+		width: fit-content;
+	}
+
+	button {
+		all: unset;
+		cursor: pointer;
+		color: var(--color-text-on-primary);
+
+		&:hover {
+			//darken the white color a bit
+			color: color-mix(in srgb, var(--color-text-on-primary) 90%, #000);
+		}
+	}
+</style>
